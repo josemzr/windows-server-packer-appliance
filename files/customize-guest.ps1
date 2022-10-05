57460bb57796f1f0575a7088e16af23dd23cb69f
Jose Manzaneque Ramos <vaulner@gmail.com>
2022-10-05 12:24:23 +0200

    Update customize-guest.ps1

--- files/customize-guest.ps1
+++ files/customize-guest.ps1
@@ -12,49 +12,87 @@ if(! (Test-Path -LiteralPath $customizationRanFile)) {
     $VMwareToolsExe = "C:\Program Files\VMware\VMware Tools\vmtoolsd.exe"
 
     [xml]$ovfEnv = & $VMwareToolsExe --cmd "info-get guestinfo.ovfEnv" | Out-String
-    $ovfProperties = $ovfEnv.ChildNodes.NextSibling.PropertySection.Property
-
-    $ovfPropertyValues = @{}
-    foreach ($ovfProperty in $ovfProperties) {
-        $ovfPropertyValues[$ovfProperty.key] = $ovfProperty.Value
-    }
-
-    # uncomment below for debugging which will output all OVF Properties to logfile
-    # $ovfProperties | Out-File -FilePath $customizationLogFile -Append
-
-    # Configure Static IP
-    if($ovfPropertyValues['guestinfo.hostname'] -ne "") {
+	
+	$vmIP = $ovfEnv.Environment.PropertySection.Property | ?{ $_.key -like '*ipaddress*' } | select -expand value
+    $vmNetmask = $ovfEnv.Environment.PropertySection.Property | ?{ $_.key -like '*netmask*' } | select -expand value
+    $vmGW = $ovfEnv.Environment.PropertySection.Property | ?{ $_.key -like '*gateway*' } | select -expand value
+    $vmHostname = $ovfEnv.Environment.PropertySection.Property | ?{ $_.key -like '*hostname*' } | select -expand value
+    $vmDNS = $ovfEnv.Environment.PropertySection.Property | ?{ $_.key -like '*dns*' } | select -expand value
+	$vmAdminPass = $ovfEnv.Environment.PropertySection.Property | ?{ $_.key -like '*adminpass*' } | select -expand value
+	
+	$vmADDomain = $vmenv.Environment.PropertySection.Property | ?{ $_.key -like '*ad_domain*' } | select -expand value
+	$vmADUsername = $vmenv.Environment.PropertySection.Property | ?{ $_.key -like '*ad_username*' } | select -expand value
+	$vmADPassword = $vmenv.Environment.PropertySection.Property | ?{ $_.key -like '*ad_password*' } | select -expand value
+	
+	# Sanitized inputs
+	# VM IP Address will have the slash removed (it is specified later as netmask)
+	# VM Netmask prefix will get the two caracters from the ovfEnv guestinfo.netmask string
+	# VM DNS will replace spaces with commas, to later feed it to the Set-DnsClientServerAddress powershell command.
+	
+	$vmIP_SAN = $vmIP.split('/')[0]
+	$vmNetmaskPrefix_SAN = $vmNetmask.split('(')[0]
+	$vmGW_SAN = $vmGW.split('/')[0]
+	$vmDNS_SAN = $vmDNS -replace(" ",",")
+	$vmADUsername_SAN = $vmADUsername.split('\')[1]
+	
+	Write-Host $vmIP_SAN
+	Write-Host $vmNetmaskPrefix_SAN
+	Write-Host $vmHostname
+	Write-Host $vmGW_SAN
+	Write-Host $vmDNS_SAN
+	Write-Host $vmAdminPass
+	Write-Host $vmADUsername_SAN
+	Write-Host $vmADDomain
+	Write-Host $vmADPassword
+	
+	# If hostname is not specified, VM will use DHCP. If it is specified, then it is mandatory to use static IP.
+	# vCenter OVFEnv will leave a blank ("") string when not specifying a parameter. However, ESXi leaves the parameter as null. Therefore it is necessary to consider that case as well.
+    if(($vmHostname -ne "") -and ($vmHostname -ne "null")){
 
         # Rename Computer & Description to match Hostname
-        "Renaming Computer Name and Description to $($ovfPropertyValues['guestinfo.hostname'])" | Out-File -FilePath $customizationLogFile -Append
-        Rename-Computer -NewName $ovfPropertyValues['guestinfo.hostname'] | Out-File -FilePath $customizationLogFile -Append
-        Get-CimInstance -ClassName Win32_OperatingSystem | Set-CimInstance -Property @{Description = $ovfPropertyValues['guestinfo.hostname']} | Out-File -FilePath $customizationLogFile -Append
+        "Renaming Computer Name and Description to $vmHostname" | Out-File -FilePath $customizationLogFile -Append
+        Rename-Computer -NewName $vmHostname | Out-File -FilePath $customizationLogFile -Append
+        Get-CimInstance -ClassName Win32_OperatingSystem | Set-CimInstance -Property @{Description = $vmHostname} | Out-File -FilePath $customizationLogFile -Append
 
         # Configure Networking
-        "Configuring IP Address to $($ovfPropertyValues['guestinfo.ipaddress'])" | Out-File -FilePath $customizationLogFile -Append
-        "Configuring Netmask to $($ovfPropertyValues['guestinfo.netmask'])" | Out-File -FilePath $customizationLogFile -Append
-        "Configuring Gateway to $($ovfPropertyValues['guestinfo.gateway'])" | Out-File -FilePath $customizationLogFile -Append
-        New-NetIPAddress –InterfaceAlias $EthernetInterfaceAliasName -AddressFamily IPv4 –IPAddress $ovfPropertyValues['guestinfo.ipaddress'] –PrefixLength $ovfPropertyValues['guestinfo.netmask'] | Out-File -FilePath $customizationLogFile -Append
-        New-NetRoute -DestinationPrefix 0.0.0.0/0 -InterfaceAlias $EthernetInterfaceAliasName -NextHop $ovfPropertyValues['guestinfo.gateway'] | Out-File -FilePath $customizationLogFile -Append
+        "Configuring IP Address to $vmIP_SAN" | Out-File -FilePath $customizationLogFile -Append
+        "Configuring Netmask Prefix to $vmNetmaskPrefix_SAN" | Out-File -FilePath $customizationLogFile -Append
+        "Configuring Gateway to $vmGW_SAN" | Out-File -FilePath $customizationLogFile -Append
+        New-NetIPAddress –InterfaceAlias $EthernetInterfaceAliasName -AddressFamily IPv4 –IPAddress $vmIP_SAN –PrefixLength $vmNetmaskPrefix_SAN_SAN | Out-File -FilePath $customizationLogFile -Append
+        New-NetRoute -DestinationPrefix 0.0.0.0/0 -InterfaceAlias $EthernetInterfaceAliasName -NextHop $vmGW_SAN | Out-File -FilePath $customizationLogFile -Append
 
         # Configure DNS
-        "Configuring DNS to $($ovfPropertyValues['guestinfo.dns'])" | Out-File -FilePath $customizationLogFile -Append
-        Set-DnsClientServerAddress -InterfaceAlias $EthernetInterfaceAliasName -ServerAddresses $ovfPropertyValues['guestinfo.dns'] | Out-File -FilePath $customizationLogFile -Append
+        "Configuring DNS to $vmDNS_SAN" | Out-File -FilePath $customizationLogFile -Append
+        Set-DnsClientServerAddress -InterfaceAlias $EthernetInterfaceAliasName -ServerAddresses $vmDNS_SAN | Out-File -FilePath $customizationLogFile -Append
         # Sleep to ensure DNS changes go into effect for AD Join
         Start-Sleep -Seconds 5
-
-        # Configure Active Directory
-        if($ovfPropertyValues['guestinfo.ad_domain'] -ne "" -and $ovfPropertyValues['guestinfo.ad_username'] -ne "" -and $ovfPropertyValues['guestinfo.ad_password'] -ne "") {
+		
+		
+		# Change local Administrator password
+		if($vmAdminPass -ne "" -and $vmAdminPass -ne "null"){
+			"Changing Administrator Password..." | Out-File -FilePath $customizationLogFile -Append
+			$SecurevmAdminPass = ConvertTo-SecureString -AsPlainText -Force $vmAdminPass
+			Get-LocalUser -Name "Administrator" | Set-LocalUser -Password $SecurevmAdminPass
+			
+			#Configure auto-login for the provided credentials
+			$RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+			Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "1" -Type String 
+			Set-ItemProperty $RegistryPath 'DefaultUsername' -Value "Administrator" -type String 
+			Set-ItemProperty $RegistryPath 'DefaultPassword' -Value "$vmAdminPass" -type String
+		}
+		
+        # Configure Active Directory. All properties must be filled in (and they must not be blank or null, to account for ESXi. See note above)
+        if($vmADDomain -ne "" -and $vmADDomain -ne "null" -and $vmADUsername_SAN -ne "" -and $vmADUsername_SAN -ne "null" -and $vmADPassword -ne "" -and $vmADPassword -ne "null") {
             $joinCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
-                UserName = $ovfPropertyValues['guestinfo.ad_username'] + "@" + $ovfPropertyValues['guestinfo.ad_domain']
-                Password = (ConvertTo-SecureString -String $ovfPropertyValues['guestinfo.ad_password'] -AsPlainText -Force)[0]
+                UserName = $vmADUsername_SAN + "@" + $vmADDomain
+                Password = (ConvertTo-SecureString -String $vmADPassword -AsPlainText -Force)[0]
             })
-            "Joining Active Directory Domain $($ovfPropertyValues['guestinfo.ad_domain'])" | Out-File -FilePath $customizationLogFile -Append
-            Add-Computer -NewName $ovfPropertyValues['guestinfo.hostname'] -Domain $ovfPropertyValues['guestinfo.ad_domain'] -Credential $joinCred -Restart | Out-File -FilePath $customizationLogFile -Append
+            "Joining Active Directory Domain $vmADDomain" | Out-File -FilePath $customizationLogFile -Append
+            Add-Computer -NewName $vmHostname -Domain $vmADDomain -Credential $joinCred -Restart | Out-File -FilePath $customizationLogFile -Append
         }
 		
 		# Reboot system to apply hostname changes
-		shutdown /r /t 5 /c "Rebooting system to apply guest customizations..."
+		#shutdown /r /t 5 /c "Rebooting system to apply guest customizations..."
     } else {
         "No OVF Properties were found, defaulting to DHCP for networking" | Out-File -FilePath $customizationLogFile -Append
     }
