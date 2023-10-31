a43169872de8704af533e9c3e3cb06932707d9af
Jose Manzaneque Ramos <vaulner@gmail.com>
2023-10-31 08:30:58 +0100

    Google Chrome gives checksum error. Ignoring checksum
    vmware-iso: ERROR: Checksum for 'C:\Users\Administrator\AppData\Local\Temp\chocolatey\GoogleChrome\118.0.5993.89\googlechromestandaloneenterprise64.msi' did not meet 'dc8035773c87a58faf1d54ac79aa0f0ea7dd18b261a63e2f5e9c372d41b94599' for checksum type 'sha256'. Consider passing the actual checksums through with --checksum --checksum64 once you validate the checksums are appropriate. A less secure option is to pass --ignore-checksums if necessary.[0m
    
    
    
    https://community.chocolatey.org/packages/GoogleChrome/87.0.4280.66

--- scripts/packages.ps1
+++ scripts/packages.ps1
@@ -1,1 +1,1 @@
-choco install -y git googlechrome dotnetfx dotnetcore 7zip kubernetes-helm kubernetes-cli notepadplusplus jre8 python putty.install packer terraform powershell-core
+choco install -y --ignore-checksums git googlechrome dotnetfx dotnetcore 7zip kubernetes-helm kubernetes-cli notepadplusplus jre8 python putty.install packer terraform powershell-core
